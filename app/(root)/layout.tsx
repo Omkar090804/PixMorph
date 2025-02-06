98ade9d1a2deb887aa99a2630013a42eb8903165
omkar darekar <omkardarekar0908@gmail.com>
2025-02-06 19:30:01 +0530

    Initial commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,19 @@
+//this containd the layout for home routes
+
+import React from 'react'
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="root">
+
+      <div className="root-container">
+        <div className="wrapper">
+      
+        </div>
+      </div>
+      {children}
+    </main>
+  )
+}
+
+export default Layout
---
+++ lib/utils.ts
@@ -0,0 +1,8 @@
+//created by shadcn
+
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
