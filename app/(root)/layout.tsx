3f50c073b78fdd1acdf7a44b2e2da6355133dcb9
omkar darekar <omkardarekar0908@gmail.com>
2025-02-07 15:41:46 +0530

    clerk final

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,17 +1,17 @@
-//this containd the layout for home routes
 
-import React from 'react'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
+    
 
       <div className="root-container">
         <div className="wrapper">
-      
+          {children}
         </div>
       </div>
-      {children}
+      
+      
     </main>
   )
 }
