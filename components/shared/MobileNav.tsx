c69d9aedb34e6fb3e5fecf7ee2820f477ca6beb1
omkar darekar <omkardarekar0908@gmail.com>
2025-03-09 08:30:20 +0530

    strong home

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const MobileNav = () => {
   return (
-    <div>MobileNav</div>
-  )
-}
+    <div>
+      <strong>Home</strong>
+    </div>
+  );
+};
 
-export default MobileNav
-  
+export default MobileNav;
