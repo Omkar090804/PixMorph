187863ad6c8012ecbe1f1c62d80cc0f77aac52a3
omkar darekar <omkardarekar0908@gmail.com>
2025-03-10 06:33:57 +0530

    creating form usiing webhook

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,19 +1,19 @@
-import Header from '@/components/shared/Header'
-import { transformationTypes } from '@/constants'
+import Header from '@/components/shared/Header';
+import { transformationTypes } from '@/constants';
+import TransformationForm from '@/components/shared/TransformationForm'; // Adjust the import path as needed
 
 const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
-  
   const transformation = transformationTypes[type];
 
   return (
-    
+    <>
       <Header 
         title={transformation.title}
         subtitle={transformation.subTitle}
       />
-    
-      
-  )
-}
+      <TransformationForm /> {/* Add the TransformationForm here */}
+    </>
+  );
+};
 
-export default AddTransformationTypePage
+export default AddTransformationTypePage;
